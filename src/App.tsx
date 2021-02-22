f02672bd066ed09217a2510fcb6000b3c2bc54c1
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-02-22 15:38:07 -0300

    feat: creating routes and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const App: React.FC = () => {
-  return <div />;
-};
+import Routes from './routes';
+
+import GlobalStyle from './style/GlobalStyle';
+
+const App: React.FC = () => (
+  <Router>
+    <Routes />
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
