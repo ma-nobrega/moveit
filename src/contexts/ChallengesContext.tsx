a3e8451b51dce662e7713a8265217ea3a2e58872
Matheus Nobrega <matheus.adao.nobrega@gmail.com>
2021-02-25 15:02:55 -0300

    feat: adding notification and sound on notification

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -31,7 +31,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [challegesCompleted, setChallegesCompleted] = useState(0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
-  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+  useEffect(()=>{
+    Notification.requestPermission();
+  },[])
 
   function levelUp() {
     setLevel(level + 1)
@@ -40,7 +44,15 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   function startNewChallenge() {
     const randomChallengeIndex = Math.floor(Math.random() * challenges.length)
     const challenge = challenges[randomChallengeIndex];
-    setActiveChallenge(challenge)
+    setActiveChallenge(challenge);
+
+    new Audio('/notification.mp3').play();
+
+    if(Notification.permission === 'granted'){
+      new Notification('Novo desafio',{
+        body: `Valendo ${challenge.amount}xp!`
+      })
+    }
   }
 
   function resetChallenge() {
